491173709e6fe2f42f0122a8c6926c7b1992dc8c
Naiara Pantuza <naiarapantuza@gmail.com>
2023-06-20 15:35:40 -0300

    primeiro commit

--- Animations/ViewController.swift
+++ Animations/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+  //MARK - IBOutlets
+  @IBOutlet weak var animatedView: UIView!
+  
+  
+  //MARK - Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
-
-
+  
+  //MARK - IBActions
+  @IBAction func startAnimationButton(_ sender: UIButton) {
+  }
 }
 
